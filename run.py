36abc7e282dab156fb6168b55c64d8158c639ca4
NelsonPixels <ebenyenelson@gmail.com>
2025-06-24 16:07:50 +0100

    dr

--- run.py
+++ run.py
@@ -8,4 +8,6 @@ def make_shell_context():
     return {'db': db, 'User': User, 'Transaction': Transaction}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
