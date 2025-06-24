7987b4ef21f6eae64d9350f1485d2d40e1978722
NelsonPixels <ebenyenelson@gmail.com>
2025-06-24 15:36:06 +0100

    dr

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
+    SECRET_KEY = '2ab10436025e2f33f2f8e8c60c55a9b2cc1734f2b2b3b2f04f7a2b0596059e50'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://postgres:mydream@localhost:5432/banking_db'
+        'postgresql://postgres:dragons24@db.tavupgrdaptcqpervtjp.supabase.co:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
