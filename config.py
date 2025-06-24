27f9b4e57a8379e5e02b53fbb4d4f85ff371e346
NelsonPixels <ebenyenelson@gmail.com>
2025-06-24 15:56:01 +0100

    dr

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     SECRET_KEY = '2ab10436025e2f33f2f8e8c60c55a9b2cc1734f2b2b3b2f04f7a2b0596059e50'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://postgres.tavupgrdaptcqpervtjp:ddrr@aws-0-us-east-2.pooler.supabase.com:5432/postgres'
+        'postgresql://postgres:ddrr@db.vsegsldvpebmyfvtgnra.supabase.co:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
